32860c3b83489130b9287e357b6a1b700bdfbe81
Giulio <giuliorelvaip@gmail.com>
2024-04-04 23:47:44 +0100

    STEP 34: World Interactions - Collectibles
    Created a health collectible, its uses and its functions!

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -6,6 +6,7 @@ public class RubyController : MonoBehaviour
 {
     public float speed = 3.0f;
     public int maxHealth = 5;
+    public int health { get { return currentHealth; }}
     int currentHealth;
     Rigidbody2D rigidbody2d;
     float horizontal; 
@@ -17,6 +18,8 @@ public class RubyController : MonoBehaviour
         rigidbody2d = GetComponent<Rigidbody2D>();
 
         currentHealth = maxHealth;
+
+        currentHealth = 1;
     }
 
     // Update is called once per frame
@@ -34,7 +37,7 @@ public class RubyController : MonoBehaviour
 
         rigidbody2d.MovePosition(position);
     }
-    void ChangeHealth (int amount)
+    public void ChangeHealth (int amount)
     
     {
         currentHealth = Mathf.Clamp (currentHealth + amount, 0, maxHealth);
